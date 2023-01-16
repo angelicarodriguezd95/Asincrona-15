e6b279fc74fd9cd51fb29f27b5ee2b37431e4c00
Rosa Rodríguez <angierodriguezd95@gmail.com>
2023-01-16 16:30:41 -0500

    angular-15

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  data: Array<any> = [];
+  newData: any;
+  nombre: string = '';
+  apellido: string = '';
+  carrera: string = '';
+  id: number = 0;
+
+  addData() {
+    this.newData = {
+      id: this.id++,
+      nombre: this.nombre,
+      apellido: this.apellido,
+      carrera: this.carrera,
+    };
+    this.data.push(this.newData);
+    this.nombre = '';
+    this.apellido = '';
+    this.carrera = '';
+    console.log(this.data);
+  }
+
+  deleteData(id: number) {
+    if (confirm('¿Estás seguro de que deseas eliminar este elemento?')) {
+      this.data = this.data.filter((item) => item.id !== id);
+    }
+  }
+  
+}
