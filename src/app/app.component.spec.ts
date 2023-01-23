dac57fa91aae13af77fa9e8eb01c114e23f9202e
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2023-01-22 21:02:24 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'actividad-9'`, () => {
+  it(`should have as title 'actividad-15'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('actividad-9');
+    expect(app.title).toEqual('actividad-15');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('actividad-9 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('actividad-15 app is running!');
   });
 });
